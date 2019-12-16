cfe8045f45faff778789a3c96a1bad94e367f90e
vinay047 <37099593+vinay047@users.noreply.github.com>
2019-12-16 00:29:25 -0800

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver  
 from selenium.webdriver.firefox.options import Options as FirefoxOptions  
 from selenium.webdriver.chrome.options import Options as ChromeOptions  
-from config import Config 
+
  
 def before_all(context):  
     # options = FirefoxOptions()  
@@ -11,7 +11,7 @@ def before_all(context):
     chrome_options.add_argument('--headless')  
     chrome_options.add_argument('--no-sandbox')  
     chrome_options.add_argument('--disable-dev-shm-usage')  
-    context.browser = webdriver.Chrome(executable_path=Config.executable_path, chrome_options=chrome_options)  
+    context.browser = webdriver.Chrome(chrome_options=chrome_options)  
       
 def after_all(context):  
     context.browser.quit() 
